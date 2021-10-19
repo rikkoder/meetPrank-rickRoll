ec3358b3c3a5a31f92885cc65f6665805b63da81
rikku <rikku.genius@gmail.com>
2021-10-20 02:31:00 +0530

    never_gonna_give_you_up

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function clicked() {
+	document.getElementById("vid").play();
+	document.getElementById("text_img").src = "../misc/never_gonna_give_you_up_2.png";
+
+	document.getElementById("ask_button").remove();
+	document.getElementById("present_button").remove();
+
+
+	
+	var never_gonna = document.createElement("img");
+	never_gonna.src = "../misc/never_gonna_give_you_up.png";
+	document.getElementById("buttons").appendChild(never_gonna);
+}
